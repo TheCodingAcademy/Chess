edd547a8a8f0eb3428c68754bcab37bb4b65d732
MaximeVandegar <maxvandegar@gmail.com>
2023-01-12 01:49:20 +0000

    implement bishop moves

--- Assets/scripts/logic/Bishop.cs
+++ Assets/scripts/logic/Bishop.cs
@@ -13,6 +13,20 @@ public class Bishop : ChessPieceLogic
     {
         bool[,] possibleMoves = new bool[BoardManager.BOARDSIZE, BoardManager.BOARDSIZE];
 
+        int[,] scale = { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1} };
+
+        for (int i = 0; i < scale.GetLength(0); i++)
+        {
+            for (int j = 1; j < 8; j++)
+            {
+                bool success = ActivateCell(possibleMoves, x + j * scale[i, 0], y + j * scale[i, 1], gameState);
+                if (!success)
+                {
+                    break;
+                }
+            }
+        }
+
         return possibleMoves;
     }
 
