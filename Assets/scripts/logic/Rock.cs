22f7aa66ebba3772f10482528118c3a21bde043c
MaximeVandegar <maxvandegar@gmail.com>
2023-01-12 01:46:19 +0000

    implement rock moves

--- Assets/scripts/logic/Rock.cs
+++ Assets/scripts/logic/Rock.cs
@@ -13,6 +13,20 @@ public class Rock : ChessPieceLogic
     {
         bool[,] possibleMoves = new bool[BoardManager.BOARDSIZE, BoardManager.BOARDSIZE];
 
+        int[,] scale = { {0, 1}, {0, -1}, { 1, 0}, { -1, 0 }};
+
+        for(int i=0; i<scale.GetLength(0); i++)
+        {
+            for (int j = 1; j < 8; j++)
+            {
+                bool success = ActivateCell(possibleMoves, x + j * scale[i, 0], y + j * scale[i, 1], gameState);
+                if (!success)
+                {
+                    break;
+                }
+            }
+        }
+
         return possibleMoves;
     }
 }
