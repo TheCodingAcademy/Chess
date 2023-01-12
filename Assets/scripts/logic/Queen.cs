dd9ee684fe7e330dbeca454b5ec9b1266dc5e084
MaximeVandegar <maxvandegar@gmail.com>
2023-01-12 01:57:06 +0000

    implement queen moves

--- Assets/scripts/logic/Queen.cs
+++ Assets/scripts/logic/Queen.cs
@@ -13,6 +13,36 @@ public class Queen : ChessPieceLogic
     {
         bool[,] possibleMoves = new bool[BoardManager.BOARDSIZE, BoardManager.BOARDSIZE];
 
+        // Diagonals
+        int[,] scale_diagonals = { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+
+        for (int i = 0; i < scale_diagonals.GetLength(0); i++)
+        {
+            for (int j = 1; j < 8; j++)
+            {
+                bool success = ActivateCell(possibleMoves, x + j * scale_diagonals[i, 0], y + j * scale_diagonals[i, 1], gameState);
+                if (!success)
+                {
+                    break;
+                }
+            }
+        }
+
+        // Forward
+        int[,] scale_forward = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+        for (int i = 0; i < scale_forward.GetLength(0); i++)
+        {
+            for (int j = 1; j < 8; j++)
+            {
+                bool success = ActivateCell(possibleMoves, x + j * scale_forward[i, 0], y + j * scale_forward[i, 1], gameState);
+                if (!success)
+                {
+                    break;
+                }
+            }
+        }
+
         return possibleMoves;
     }
 }
